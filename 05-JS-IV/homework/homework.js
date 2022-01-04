4e9df5afe99ea7b88c54ebe564b3134ddedc4489
BugalloF <bugallof@gmail.com>
2022-01-04 20:05:11 -0300

    prueba

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,7 +6,15 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
-}
+  var Gato = {
+    nombre : nombre ,
+    edad : edad,
+    function meow () {
+      return 'Meow!'
+    }
+  }
+  return Gato
+  }
 
 
 function agregarPropiedad(objeto, property) {
@@ -14,7 +22,7 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-}
+  }
 
 function invocarMetodo(objeto, metodo) {
   // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
