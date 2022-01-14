44e349d2f96d7d791956ca702b432acfaa431635
BugalloF <bugallof@gmail.com>
2022-01-14 10:59:56 -0300

    HW X FINISHED

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -108,9 +108,11 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+      const intersectionResult = arreglo1.filter(x => arreglo2.indexOf(x) !== -1);
+ 
+    return intersectionResult
+
 }
-
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
